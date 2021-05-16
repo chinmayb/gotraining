71772009339735dbc1ec2922fc72a404a47843de
chinmay <cbharadwaj@infoblox.com>
2021-05-17 00:28:41 +0530

    move to separate repo

--- cobraproject/pkg/errorutil/errors.go
+++
@@ -1,8 +0,0 @@
-package errorutil
-
-type ErrorBody struct {
-	Error   string `json:"error,omitempty"`
-	Message string `json:"message,omitempty"`
-	Details string `json:"details,omitempty"`
-	ErrorCode
-}
